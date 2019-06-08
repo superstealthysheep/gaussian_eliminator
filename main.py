665cad9a9baf8d5174aa01de9836e4e0154b41c3
William Walker <24390822+superstealthysheep@users.noreply.github.com>
2019-06-08 02:07:42 -0500

    Fixed camel case
    Also made the program print the input matrix nicely once it's received it.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 matrix_height = 3
 matrix_width = 4
 
-def takeInput(): 
+def take_input(): 
   
   
   matrix = []
@@ -22,7 +22,7 @@ def takeInput():
   return(matrix)
 
 
-def printMatrix(matrix):
+def print_matrix(matrix):
   print("[")
   for row in matrix:
     print(row)
@@ -30,7 +30,7 @@ def printMatrix(matrix):
 
 
 #multiplies a row by a scalar
-def multiplyRow(row_vector, scalar):
+def multiply_row(row_vector, scalar):
   output_row_vector = []
 
   #does it element-wise
@@ -39,7 +39,7 @@ def multiplyRow(row_vector, scalar):
   return(output_row_vector)
 
 #adds two rows
-def addRows(row_vector_1, row_vector_2):
+def add_rows(row_vector_1, row_vector_2):
   output_row_vector = []
 
   #does it element_wise
@@ -48,31 +48,31 @@ def addRows(row_vector_1, row_vector_2):
 
   return(output_row_vector)
 
-def swapRows(matrix, rowNumber1, rowNumber2):
-  row1 = matrix[rowNumber1]
-  row2 = matrix[rowNumber2]
+def swap_rows(matrix, row_number_1, row_number_2):
+  row1 = matrix[row_number_1]
+  row2 = matrix[row_number_2]
   
-  matrix.pop(rowNumber1)
-  matrix.insert(rowNumber1, row2)
+  matrix.pop(row_number_1)
+  matrix.insert(row_number_1, row2)
 
-  matrix.pop(rowNumber2)
-  matrix.insert(rowNumber2, row1)
+  matrix.pop(row_number_2)
+  matrix.insert(row_number_2, row1)
 
   return matrix
 
 
 #Takes matrix, then tries to find pivot row 
 #swaps the pivot row into the working_column number-th row, then returns if it was successful.
-def findPivotRow(matrix, working_column_number):
+def find_pivot_row(matrix, working_column_number):
   
-  for possiblePivotRowNumber in range(working_column_number, matrix_height):
-    print("Testing row {}, column {}".format(possiblePivotRowNumber, working_column_number))
-    possiblePivotRow = matrix[possiblePivotRowNumber]
+  for possible_pivot_row_number in range(working_column_number, matrix_height):
+    print("Testing row {}, column {}".format(possible_pivot_row_number, working_column_number))
+    possible_pivot_row = matrix[possible_pivot_row_number]
 
-    if possiblePivotRow[working_column_number] == 0:
+    if possible_pivot_row[working_column_number] == 0:
       continue
     else:
-      swapRows(matrix, working_column_number, possiblePivotRowNumber)
+      swap_rows(matrix, working_column_number, possible_pivot_row_number)
       pivot_found = True
       return (matrix, pivot_found)
 
@@ -82,13 +82,13 @@ def findPivotRow(matrix, working_column_number):
   return (matrix, pivot_found)
 
 
-def doGaussianElimination(matrix):
+def do_gaussian_elimination(matrix):
 
   #The min is because you can only have as many potential pivots as there are diagonal entries (which is equal to the min of matrix width and length)
   for working_column_number in range(0, min(matrix_width, matrix_height)):
     
     #adjusts matrix until a possible pivot for this column is in the row with number equal to the working_column_number
-    find_pivot_row_results = findPivotRow(matrix, working_column_number)
+    find_pivot_row_results = find_pivot_row(matrix, working_column_number)
     matrix = find_pivot_row_results[0]
     pivot_found = find_pivot_row_results[1]
     #If a pivot is not found, skip this column.
@@ -96,17 +96,17 @@ def doGaussianElimination(matrix):
       print("Continuing, because pivot was not found")
       continue
 
-    #this is true because the findPivotRow step moves the pivot row into the correct row.
+    #this is true because the find_pivot_row step moves the pivot row into the correct row.
     pivot_row_number = working_column_number
 
     #sets up pivot_row
     pivot_row = matrix[pivot_row_number]
     #multiplies pivot_row by scalar such that it actually has a pivot.
-    pivot_row = multiplyRow(pivot_row, 1/(pivot_row[working_column_number]))
+    pivot_row = multiply_row(pivot_row, 1/(pivot_row[working_column_number]))
     #Possible error: divide by zero if the matrix has any zeros along its diagonal
 
     """for non_pivot_row in (matrix.pop(working_column_number)):
-      addRows(non_pivot_row, multiplyRow(pivot_row, -non_pivot_row[working_column_number]))"""
+      add_rows(non_pivot_row, multiply_row(pivot_row, -non_pivot_row[working_column_number]))"""
 
     #adds ("adjusts") rows so that all rows except for the pivot row have an entry of 0 in the working column
     for row_number in range(0, matrix_height):
@@ -116,7 +116,7 @@ def doGaussianElimination(matrix):
       else:
         current_row = matrix[row_number]
         #subtracts correct multiple of pivot_row from current_row
-        adjusted_row = addRows(current_row, multiplyRow(pivot_row, -1 * current_row[working_column_number]))
+        adjusted_row = add_rows(current_row, multiply_row(pivot_row, -1 * current_row[working_column_number]))
 
         #replace current_row in matrix with adjusted_row
         matrix.pop(row_number)
@@ -124,7 +124,7 @@ def doGaussianElimination(matrix):
 
   return matrix
 
-#takeInput()
+#take_input()
 
 """
 #input_matrix = [[2, 1, 1],
@@ -138,4 +138,9 @@ input_matrix = [[0, 0, 1],
                 [0, 1, 0]]
 """
 
-printMatrix(doGaussianElimination(takeInput()))
+input_matrix = take_input()
+print("Input:")
+print_matrix(input_matrix)
+
+print("\nReducing...")
+print_matrix(do_gaussian_elimination(input_matrix))
